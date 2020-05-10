cca35256f0b088f68cb37cf45f35fbc28aa77d02
Laks Castro <lakhsmycastro336@gmail.com>
2020-05-10 18:44:13 -0300

    Update keyboard.ts

--- src/scripts/keyboard.ts
+++ src/scripts/keyboard.ts
@@ -1,1 +1,31 @@
+import hotkeys from 'hotkeys-js';
 
+import ApplicationCalculator from "./calculator";
+
+interface IKeyboardMethods {
+  initialize: () => void;
+} 
+
+type Keyboard = () => Readonly<IKeyboardMethods>
+
+const Keyboard : Keyboard = () => {
+  
+  const initialize = () => {
+    for(let i = 0; i <= 9; i++){
+      hotkeys(i.toString(), () => ApplicationCalculator.addText(i.toString()));
+    }
+    hotkeys('-', () => ApplicationCalculator.addText("-"));
+    hotkeys('+', () => ApplicationCalculator.addText("+"));
+    hotkeys('*', () => ApplicationCalculator.addText("×"));
+    hotkeys('/', () => ApplicationCalculator.addText("÷"));
+    hotkeys(',', () => ApplicationCalculator.addText("."));
+    hotkeys('.', () => ApplicationCalculator.addText("."));
+    hotkeys('backspace', () => ApplicationCalculator.del());
+  }
+  
+  const self = {
+    initialize
+  }
+  
+  return Object.freeze(self);
+}
